4f09ddeb1d53990762f7c3c77891fdf7ceba3a2e
yakimka <ss.yakim@gmail.com>
2025-10-12 02:56:27 +0200

    Remove validation functions

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,8 +4,6 @@ import sys
 PROJECT_REGEX = r"^[a-z][a-z0-9\-_]+[a-z0-9]$"
 PROJECT_NAME = "{{ cookiecutter.project_name }}"
 MODULE_NAME = "{{ cookiecutter.module_name }}"
-LINTER = "{{ cookiecutter.linter }}"
-DEPENDENCY_UPDATER = "{{ cookiecutter.dependency_updater }}"
 MINIMAL_PYTHON_VERSION = "{{ cookiecutter.minimal_python_version }}"
 MAXIMUM_PYTHON_VERSION = "{{ cookiecutter.maximum_python_version }}"
 
@@ -38,26 +36,6 @@ def validate_module_name():
         raise ValueError(" ".join(message).format(MODULE_NAME))
 
 
-def validate_linter():
-    valid_linters = ("ruff", "flake8")
-    if LINTER not in valid_linters:
-        message = [
-            "ERROR: The linter {0} is not a valid option.",
-            "Choose one of the following: ruff, flake8",
-        ]
-        raise ValueError(" ".join(message).format(LINTER))
-
-
-def validate_dependency_updater():
-    valid_updaters = ("dependabot", "renovate", "none")
-    if DEPENDENCY_UPDATER not in valid_updaters:
-        message = [
-            "ERROR: The dependency updater {0} is not a valid option.",
-            "Choose one of the following: dependabot, renovate, none",
-        ]
-        raise ValueError(" ".join(message).format(DEPENDENCY_UPDATER))
-
-
 def validate_python_version():
     _check_py_version(MINIMAL_PYTHON_VERSION)
     _check_py_version(MAXIMUM_PYTHON_VERSION)
@@ -84,8 +62,6 @@ def _check_py_version(version: str) -> None:
 validators = (
     validate_project_name,
     validate_module_name,
-    validate_linter,
-    validate_dependency_updater,
     validate_python_version,
 )
 
